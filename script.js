7748de6a6d9ccdd1df442c9948b28c390875936e
durga2677 <125868369+durga2677@users.noreply.github.com>
2023-07-12 16:33:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function fibonacci(num) {
-// your code here
-}
 
+let a=0;b=1;
+function fibonacci(n) {
+	
+// your code here
+	if(n<=2){
+	return n-1;
+	}
+	for(let i=;i<=n-2;i++){
+		let temp=b;
+		b=a+b;
+		a=temp;
+	}
+	return b;
+}
+console.log(fibonacci(49))
 module.exports = fibonacci;
