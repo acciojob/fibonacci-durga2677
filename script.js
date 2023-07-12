e564051c9c7fe1a0b0e492520553bf33ad52539a
durga2677 <125868369+durga2677@users.noreply.github.com>
2023-07-12 16:38:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 let a=0;b=1;
-function fibonacci(n) {
+function fibonacci(num) {
 	
 // your code here
-	if(n<=2){
-	return n-1;
+	if(num<=2){
+	return num-1;
 	}
-	for(let i=1;i<=n-2;i++){
+	for(let i=1;i<=num-2;i++){
 		let temp=b;
 		b=a+b;
 		a=temp;
